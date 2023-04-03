a06f3f98d48bb781cb9a3c977ac7775b9a761380
pushpraj paroha <satyamparoha786@gmail.com>
2023-04-04 00:19:27 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 // creation of express server
 const express = require("express");
+//dotenv install
 const dotenv= require("dotenv").config();
+
+//thunder client extension
 const app = express();
+//get method
+app.get('/api/contacts',(req,res)=>{
+    res.send("Get for contact")
+})
 const port = process.env.PORT;
 app.listen(port,()=>{
     console.log(`server running on ${port}` )
