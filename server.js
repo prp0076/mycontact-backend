47f7fe8e760e3928731338d53471a8937f35ecda
pushpraj paroha <satyamparoha786@gmail.com>
2023-04-04 00:38:03 +0530

    initial commit

--- server.js
+++ server.js
@@ -6,9 +6,14 @@ const dotenv= require("dotenv").config();
 //thunder client extension
 const app = express();
 //get method
-app.get('/api/contacts',(req,res)=>{
-    res.send("Get for contact")
-})
+// app.get("/api/contacts",(req,res)=>{
+//     res.send("Get for contact");
+// })
+
+//json response
+app.get("/api/contacts",(req,res)=>{
+  res.json({message:"get all contacts"});
+}) 
 const port = process.env.PORT;
 app.listen(port,()=>{
     console.log(`server running on ${port}` )
