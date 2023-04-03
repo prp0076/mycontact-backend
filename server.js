45f2c6a9e90c441921e50fd957b152bebadea149
pushpraj paroha <satyamparoha786@gmail.com>
2023-04-04 00:12:29 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+// creation of express server
+const express = require("express");
+const dotenv= require("dotenv").config();
+const app = express();
+const port = process.env.PORT;
+app.listen(port,()=>{
+    console.log(`server running on ${port}` )
+})
