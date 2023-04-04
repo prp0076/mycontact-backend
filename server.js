403165811dc64ac1ae2ddb2d0d6663fb9ed79a43
pushpraj paroha <satyamparoha786@gmail.com>
2023-04-05 01:21:23 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // creation of express server
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 //dotenv install
 const dotenv= require("dotenv").config();
  
@@ -8,6 +9,8 @@ const app = express();
 //app.use is middleware
 app.use(express.json());
 app.use("/api/contacts",require("./routes/contactRoutes"));
+app.use(errorHandler);
+
 //get method
 // app.get("/api/contacts",(req,res)=>{
 //     res.send("Get for contact");
