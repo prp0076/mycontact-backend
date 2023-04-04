2e1f10f73e0f33dbe14c9d127a1ea16635d4b929
pushpraj paroha <satyamparoha786@gmail.com>
2023-04-04 22:19:24 +0530

    initial commit

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -4,13 +4,13 @@ const router = express.Router();
 router.route("/").get((req,res)=>{
        res.status(200).json({message:"get all contacts"});
 });
-router.route("/").get((req,res)=>{
+router.route("/").post((req,res)=>{
        res.status(200).json({message:"get all contacts"});
 });
-router.route("/").get((req,res)=>{
-       res.status(200).json({message:"get all contacts"});
+router.route("/:id").put((req,res)=>{
+       res.status(200).json({message:`update contact ${req.params.id}`});
 });
-router.route("/").get((req,res)=>{
-       res.status(200).json({message:"get all contacts"});
+router.route("/:id").get((req,res)=>{
+       res.status(200).json({message:`delete all contacts ${req.params.id}`});
 });
 module.exports=router
