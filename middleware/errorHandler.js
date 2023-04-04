dd641e8fa7293594f87d0fad90a4e3f3885ce08f
pushpraj paroha <satyamparoha786@gmail.com>
2023-04-05 01:36:01 +0530

    initial commit

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,11 +1,11 @@
+const contant = require("../constant");
 const errorHandler =(err,req,res,next)=>{
     const statusCode = res.statusCode ? res.statusCode : 500;
     switch(statusCode){
-    case 400:
+    case contant.VALIDATION_ERROR:
     res.json({tittle:"validation failed",message:err.message,stackTrace:err.stack});
-    case 404:
+    case contant.NOTFOUND:
      res.json({tittle:"Not Found",message:err.message,stackTrace:err.stack})
-
     }
 };
 module.exports=errorHandler
