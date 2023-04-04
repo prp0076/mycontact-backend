15b1229eccda88882766aff10e2e4bc6d381ef98
pushpraj paroha <satyamparoha786@gmail.com>
2023-04-05 01:49:55 +0530

    initial commit

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,12 +1,13 @@
+const asyncHandler = require("express-async-handler");
+
 //desc Get all contact
 //@route Get /api/contacts
 //@access public
 
-
-const getContact= async (req,res)=>{
+const getContact= asyncHandler(async (req,res)=>{
     res.status(200).json({message:"get all contacts"});
-}
-const postContact=async (req,res)=>{
+})
+const postContact= asyncHandler(async (req,res)=>{
     console.log(req.body);
     const {name,age,contact}=req.body;
     if(!name || !age || !contact){
@@ -14,12 +15,12 @@ const postContact=async (req,res)=>{
         throw new Error("All field are mendatory");
     }
     res.status(200).json({message:"post all contacts"});
-}
-const putContact=async (req,res)=>{
+})
+const putContact=asyncHandler(async (req,res)=>{
     res.status(200).json({message:`update contact ${req.params.id}`});
-}
-const deleteContact=async (req,res)=>{ 
+})
+const deleteContact=asyncHandler(async (req,res)=>{ 
     res.status(200).json({message:`delete all contacts ${req.params.id}`});
-}
+})
 
 module.exports ={getContact,postContact,putContact,deleteContact}
