123071cd53a02f0b253fbd3270d1a1a6e5c45c44
pushpraj paroha <satyamparoha786@gmail.com>
2023-04-05 19:18:23 +0530

    initial commit

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const {getContact,postContact,putContact,deleteContact}=require("../controllers/contactController");
 
-router.route("/").get(getContact).post(postContact);
-// router.route("/").post(postContact);
-router.route("/:id").put(putContact).delete(deleteContact);
+router.route("/").get(getContact);
+router.route("/").post(postContact);
+router.route("/:id").put(putContact);
 // router.route("/:id").get();
-// router.route("/:id").delete(deleteContact);
+router.route("/:id").delete(deleteContact);
 module.exports=router
